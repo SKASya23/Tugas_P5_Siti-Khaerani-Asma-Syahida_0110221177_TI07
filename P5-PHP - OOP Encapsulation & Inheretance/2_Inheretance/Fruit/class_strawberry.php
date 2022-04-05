876c1da6d2d2a8bb1effb64891419091bf4286e7
SKASya23 <91138409+SKASya23@users.noreply.github.com>
2022-04-05 09:40:52 +0700

    Add files via upload

--- P5-PHP - OOP Encapsulation & Inheretance/2_Inheretance/Fruit/class_strawberry.php
+++ P5-PHP - OOP Encapsulation & Inheretance/2_Inheretance/Fruit/class_strawberry.php
@@ -1,1 +1,17 @@
+<?php
+use Strawberry as Strawberry;
 
+require 'class_fruit(2.1).php';
+
+class Strawberry extends Fruit{
+    public function message()
+    {
+        echo "Am I a fruit or a berry? ";
+    }
+}
+
+$set_strawberry = new Strawberry("Strawberry", "red");
+$set_strawberry->message();
+$set_strawberry->intro();
+
+?>
