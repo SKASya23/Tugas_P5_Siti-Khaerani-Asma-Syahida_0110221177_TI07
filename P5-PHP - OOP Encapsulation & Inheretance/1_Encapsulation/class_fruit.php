b493201c5fa2f26c645fbf612d2d840362996332
SKASya23 <91138409+SKASya23@users.noreply.github.com>
2022-04-05 09:32:25 +0700

    Add files via upload

--- P5-PHP - OOP Encapsulation & Inheretance/1_Encapsulation/class_fruit.php
+++ P5-PHP - OOP Encapsulation & Inheretance/1_Encapsulation/class_fruit.php
@@ -1,1 +1,21 @@
-# Tugas_P5_Siti-Khaerani-Asma-Syahida_0110221177_TI07
+<?php
+class Fruit {
+    public $name;
+    public $color;
+    public $weight;
+    function name($n) {
+        $this -> name = $n;
+    }
+    protected function color($n) {
+        $this -> color = $n;
+    }
+    private function weight($n) {
+        $this -> weight - $n;
+    }
+}
+$mango = new Fruit();
+$mango -> name = 'Mango';
+$mango -> color =  'Yellow';
+$mango -> weight = '300';
+
+?>
